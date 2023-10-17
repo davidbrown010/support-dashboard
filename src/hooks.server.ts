8d69a379cd137743552277e3fda658a45702f41a
David Brown <davidbrown010@comcast.net>
2023-10-17 02:17:01 -0600

    changes for non-cookied enabled api requests

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -8,15 +8,17 @@ export const handle = (async ({ event, resolve }) => {
     const nonAuthPages = ['/login', '/register', '/resetPassword', '/login/api', '/logout/api']
     
     if (nonAuthPages.indexOf(event.url.pathname) == -1) {
-        const session = await event.locals.auth.validate();
+        let session = await event.locals.auth.validate();
 
 	    if (!session) {
             //checks for API usage
             const authorizationHeader = event.request.headers.get("Authorization");
-            const session = await event.locals.auth.validateBearerToken();
+            session = await event.locals.auth.validateBearerToken();
 
-            if (event.url.pathname.includes("api")) return json({status: 403, message: "Unauthorized."})
-            throw redirect(302, `/login?redirect=${encodeURI(event.url.pathname)}`);
+            if (!session) {
+                if (event.url.pathname.includes("api")) return json({status: 403, message: "Unauthorized."})
+                throw redirect(302, `/login?redirect=${encodeURI(event.url.pathname)}`);
+            }
         }
 
         event.locals.user = {
