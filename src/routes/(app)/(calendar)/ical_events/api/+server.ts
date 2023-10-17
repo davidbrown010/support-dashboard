e472c4bc0d074279e7f96156a8eb960d9bdd51d1
David Brown <davidbrown010@comcast.net>
2023-10-17 16:44:33 -0600

    added breakdown dimension for date or event

--- src/routes/(app)/(calendar)/ical_events/api/+server.ts
+++ src/routes/(app)/(calendar)/ical_events/api/+server.ts
@@ -1,8 +1,44 @@
 import { getAllICalEvents } from '$lib/server/db/icalEvents/handler';
 import type { RequestHandler } from './$types';
 import { json } from '@sveltejs/kit';
+import type { icalEvent } from '$lib/server/db/icalEvents/helpers/parser';
 
-export const GET: RequestHandler =  async () => {
+export const GET: RequestHandler =  async ({url}) => {
+    const breakdownDimension = url.searchParams.get("breakdownDimension")
+
     const allEvents = await getAllICalEvents()
-    return json(allEvents);
+
+    if (breakdownDimension == "event") return json(allEvents)
+    else if (breakdownDimension == "date") {
+        let daysAr = [] as allDaysHolder[]
+
+        for (const event of allEvents) {
+            const indexOfDaysAr = daysAr.map(x=>x.date).indexOf(event.start.toDateString())
+            if (indexOfDaysAr == -1) {
+                daysAr.push({
+                    date: event.start.toDateString(),
+                    events: [event]
+                })
+            } else {
+                daysAr[indexOfDaysAr].events.push(event)
+            }
+        }
+
+        return json(daysAr.map(x=>{
+            return {
+                date: new Date(x.date),
+                length: x.events.reduce((agg, cur) => {
+                    return agg + cur.length
+                }, 0)
+            }
+        }))
+
+    }
+    
+    return json(allEvents)
 };
+
+type allDaysHolder = {
+    date: string
+    events: icalEvent[]
+}
