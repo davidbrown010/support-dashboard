f6b20af16c45ac3a8a213e6c4002e7e81db070d1
David Brown <davidbrown010@comcast.net>
2024-03-26 17:16:39 -0600

    finalized calendar stats

--- src/lib/server/db/icalEvents/helpers/quickStats.ts
+++ src/lib/server/db/icalEvents/helpers/quickStats.ts
@@ -2,8 +2,6 @@ import type {formatted_icalEvent} from "./parser"
 
 export const sumHours = (icalEvents: formatted_icalEvent[]) => {
     const totalHoursSummed: number = icalEvents.reduce((agg: number, cur: formatted_icalEvent) => {
-        console.log(cur.name + ' - ' + cur.length)
-        console.log(agg)
         return agg + cur.length
     }, 0)
 
--- src/routes/api/calendar_stats/+server.ts
+++ src/routes/api/calendar_stats/+server.ts
@@ -19,8 +19,6 @@ export const GET: RequestHandler =  async ({url, locals}) => {
 
     const sumStats = sumHours(allEvents)
 
-    console.log(sumStats)
-
     return json({
         sumStats
     })
