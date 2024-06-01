1e5a224462798979acf97ec37792bab7256faae1
David Brown <davidbrown010@comcast.net>
2024-06-01 03:56:31 -0600

    removed api keys temporarily

--- src/lib/server/db/api_keys/handler.ts
+++ src/lib/server/db/api_keys/handler.ts
@@ -1,47 +1,47 @@
-import { db } from "../drizzle"
-import { eq, and } from "drizzle-orm"
-import { apiKeysTable } from "./schema"
+// import { db } from "../drizzle"
+// import { eq, and } from "drizzle-orm"
+// import { apiKeysTable } from "./schema"
 
-const { subtle } = globalThis.crypto;
+// const { subtle } = globalThis.crypto;
 
-async function generateAesKey(length = 256) {
-  const key = await subtle.generateKey({
-    name: 'AES-CBC',
-    length,
-  }, true, ['encrypt', 'decrypt']);
+// async function generateAesKey(length = 256) {
+//   const key = await subtle.generateKey({
+//     name: 'AES-CBC',
+//     length,
+//   }, true, ['encrypt', 'decrypt']);
 
-  return subtle.exportKey('jwk', key);
-} 
+//   return subtle.exportKey('jwk', key);
+// } 
 
-const insertApiKey = async (userId: string) => {
+// const insertApiKey = async (userId: string) => {
 
-    const getNewDate = () => {
-        const date = new Date();
-        date.setFullYear(date.getFullYear() + 1)
-        return date.valueOf();
-    }
+//     const getNewDate = () => {
+//         const date = new Date();
+//         date.setFullYear(date.getFullYear() + 1)
+//         return date.valueOf();
+//     }
 
-    const JWK = await generateAesKey()
+//     const JWK = await generateAesKey()
     
-    const apiKey = JWK.k
+//     const apiKey = JWK.k
 
-    return db.insert(apiKeysTable).values({
-        userId,
-        apiKey: apiKey!,
-        expires: getNewDate()
-    });
-}
+//     return db.insert(apiKeysTable).values({
+//         userId,
+//         apiKey: apiKey!,
+//         expires: getNewDate()
+//     });
+// }
 
-export const createApiKey = async (userId: string) => {
-    const newApiKey = await insertApiKey(userId);
-    return newApiKey
-}
+// export const createApiKey = async (userId: string) => {
+//     const newApiKey = await insertApiKey(userId);
+//     return newApiKey
+// }
 
-export const getApiKeys = async (userId: string) => {
-    return await db.select().from(apiKeysTable).where(eq(apiKeysTable.userId, userId))
-}
+// export const getApiKeys = async (userId: string) => {
+//     return await db.select().from(apiKeysTable).where(eq(apiKeysTable.userId, userId))
+// }
 
-export const removeApiKey = async (userId: string, apiKeyId: string) => {
-    const apiKeyId_int = parseInt(apiKeyId)
-    return await db.delete(apiKeysTable).where(and(eq(apiKeysTable.id, apiKeyId_int), eq(apiKeysTable.userId, userId)));
-}
+// export const removeApiKey = async (userId: string, apiKeyId: string) => {
+//     const apiKeyId_int = parseInt(apiKeyId)
+//     return await db.delete(apiKeysTable).where(and(eq(apiKeysTable.id, apiKeyId_int), eq(apiKeysTable.userId, userId)));
+// }
